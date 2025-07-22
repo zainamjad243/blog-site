1e30591aff5294ef5d1eebca97660edc0b00cef1
zainamjad243 <zain243a@gmail.com>
2025-07-22 11:16:37 +0500

    Fix all

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],git
+  plugins: [react()],
+  base: process.env.VITE_BASE_PATH || "/blog-site",
 })
