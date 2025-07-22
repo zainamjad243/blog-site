ff0aab720d1000f37879c7685ddd06e03ddc880f
zainamjad243 <zain243a@gmail.com>
2025-07-22 11:08:26 +0500

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "/Blog-website",
+  plugins: [react()],git
 })
